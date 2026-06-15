c8ca79df363bbaf54326557f79374a4bb931361e
Sheik Rifaz Ali <alirifaz21@gmail.com>
2026-06-15 17:54:02 +0530

    Implement higher order component for managing windows

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { Draggable } from 'gsap/Draggable';
+gsap.registerPlugin(Draggable)
+import gsap from 'gsap';
 
 import { Navbar, Welcome, Dock } from '#components'
+import { Terminal } from '#windows';
 
 const App = () => {
   return (
@@ -8,6 +12,8 @@ const App = () => {
       <Navbar />
       <Welcome />
       <Dock />
+
+      <Terminal/>
     </main>
   )
 }
