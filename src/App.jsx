d8c36530bb5cab346216652230b2a7c07a0da27c
Sheik Rifaz Ali <alirifaz21@gmail.com>
2026-06-15 14:32:45 +0530

    implemented the doc feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
-import { Navbar, Welcome } from '#components'
+import { Navbar, Welcome, Dock } from '#components'
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <Welcome />
+      <Dock />
     </main>
   )
 }
