77677d756f2b9cad4bf0cc5e987d263294f059bb
Sheik Rifaz Ali <alirifaz21@gmail.com>
2026-06-15 18:29:28 +0530

    implemented the blog section with safari browser ui

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ gsap.registerPlugin(Draggable)
 import gsap from 'gsap';
 
 import { Navbar, Welcome, Dock } from '#components'
-import { Terminal } from '#windows';
+import { Terminal, Safari } from '#windows';
 
 const App = () => {
   return (
@@ -14,6 +14,7 @@ const App = () => {
       <Dock />
 
       <Terminal/>
+      <Safari/>
     </main>
   )
 }
