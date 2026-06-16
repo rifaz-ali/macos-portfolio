c1b8432db785842409c7bb7f0503314e482f51fe
Sheik Rifaz Ali <alirifaz21@gmail.com>
2026-06-16 07:09:44 +0530

    Implemented resume section with windowWrapper

---
+++ src/store/location.js
@@ -0,0 +1,17 @@
+const { locations } = require("#constants");
+const { create } = require("zustand");
+const { immer } = require("zustand/middleware/immer");
+
+const DEFAULT_LOCATION = locations.work;
+
+const useLocationStore = create(immer((set) => ({
+    activeLocation : DEFAULT_LOCATION,
+
+    setActiveLocation: (location = null) => set((state) => {
+        state.activeLocation = location;
+    }),
+
+    resetActiveLocation: () => set((state) => {
+        state.activeLocation = DEFAULT_LOCATION;
+    })
+})))
